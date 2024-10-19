98215820afff8f34f328aaad7bde4649f5923fbb
shahid Afridi <shahidafridi.baymax@gmail.com>
2024-10-19 11:48:12 +0600

    create chapter moduele

---
+++ pages/program-management/Chapter.js
@@ -0,0 +1,28 @@
+class Chapter {
+  constructor(page) {
+    this.page = page;
+    this.programManagementTab = page.getByText("Program Management");
+    this.chapterTab = page.getByRole("link", { name: "Chapter" });
+    this.addChapterBtn = page.getByRole("button", { name: "Add Chapter" });
+    this.chapterNameInput = page.getByLabel("chapter Name");
+    this.classInput = page.getByLabel("Class");
+    this.selectClass = page.getByTitle("Admission").locator("div");
+    this.course = page.getByLabel("Course");
+    this.selectCourse = page.getByText("ইঞ্জিনিয়ারিং পদার্থবিজ্ঞান");
+    this.subject = page.getByLabel("Subject");
+    this.selectSubject = page
+      .getByTitle("পদার্থবিজ্ঞান ১ম পত্র")
+      .locator("div");
+    this.serialNumberInput = page.getByLabel("Serial Number");
+    this.submitBtn = page.getByRole("button", { name: "Submit" });
+    this.searchField = page.getByPlaceholder("Search by name");
+    this.editBtn = page.getByRole("button", { name: "edit" });
+    this.inactiveBtn = page.getByText("Inactive");
+    this.updateBtn = page.getByRole("button", { name: "Update" });
+    this.statusFilter = page.locator("#rc_select_3");
+    this.selectInactive = page.getByTitle("Inactive").locator("div");
+    this.deleteBtn = page.getByRole("button", { name: "delete" });
+    this.confirmDelete = page.getByRole("button", { name: "OK" });
+  }
+}
+export default Chapter;
---
+++ tests/ChapterTestRunner.spec.js
@@ -0,0 +1,56 @@
+import { test, expect } from "@playwright/test";
+import Chapter from "../pages/program-management/Chapter";
+import { login } from "../pages/AuthHelper";
+
+test.describe("Program Management -> Chapter", () => {
+  test.beforeEach(async ({ page }) => {
+    await login(page);
+    const chapter = new Chapter(page);
+    await chapter.programManagementTab.click();
+    await chapter.chapterTab.click();
+    await expect(
+      page.getByRole("heading", { name: "Chapter List" })
+    ).toBeVisible();
+    await page.waitForLoadState("networkidle");
+  });
+
+  test("Admin can create chapter successfully", async ({ page }) => {
+    const createChapter = new Chapter(page);
+
+    await createChapter.addChapterBtn.click();
+    await createChapter.chapterNameInput.fill("automation");
+    await createChapter.classInput.click();
+    await createChapter.selectClass.click();
+    await createChapter.course.click();
+    await createChapter.selectCourse.click();
+    await createChapter.subject.click();
+    await createChapter.selectSubject.click();
+    await createChapter.serialNumberInput.fill("99");
+    await createChapter.submitBtn.click();
+    await expect(page.getByText("Successfully created")).toBeVisible();
+  });
+
+  test("Admin can edit Chapter successfully", async ({ page }) => {
+    const editChapter = new Chapter(page);
+
+    await editChapter.searchField.fill("automation");
+    await page.waitForTimeout(1000);
+    await editChapter.editBtn.click();
+    await editChapter.inactiveBtn.click();
+    await editChapter.updateBtn.click();
+
+    await expect(page.getByText("Successfully updated")).toBeVisible();
+  });
+
+  test("Admin can delete Chapter successfully", async ({ page }) => {
+    const deleteChapter = new Chapter(page);
+
+    await deleteChapter.statusFilter.click();
+    await deleteChapter.selectInactive.click();
+    await page.waitForTimeout(1000);
+    await deleteChapter.deleteBtn.click();
+    await deleteChapter.confirmDelete.click();
+
+    await expect(page.getByText("Successfully deleted")).toBeVisible();
+  });
+});
